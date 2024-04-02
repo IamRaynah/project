9d8fdf8d8d7e74861bfddce01e2350cce18fa35e
Reinhardt <reinhardtcollins@live.com>
2024-04-02 03:20:53 +0300

    alert

--- src/App.tsx
+++ src/App.tsx
@@ -105,7 +105,7 @@ const App = () => {
         <div className="w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
             {
                 error &&
-                <div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
+                <div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg lg:overflow-visible">
                     <div role="alert" className="relative block w-full text-base font-regular px-4 py-4 rounded-lg bg-red-500 text-white flex">
                         <div className=" mr-12">
                             <p className="font-bold text-white">
